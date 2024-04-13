73ba7566f8c994241dbcb0182a090367dddc2755
SeniorMooMoo <mohammadqadervb@gmail.com>
2024-04-13 14:37:04 -0400

    Changed landing page / removed accessibility restrictions / removed calendly

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,92 +1,69 @@
-import { Button } from "@/components/ui/button";
-import { ArrowRight } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
+"use client";
+import { ArrowRight, Code, ImageIcon, MessageSquare, Music, VideoIcon } from "lucide-react";
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from 'react';
 
-export default function Home() {
+const tools = [
+  {
+    label:"Coversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation"
+  },
+]
+
+const DashboardPage = ()  => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://assets.calendly.com/assets/external/widget.js';
+    script.async = true;
+
+
+    document.head.appendChild(script);
+
+    return () => {
+      // Cleanup when the component unmounts
+      document.head.removeChild(script);
+    };
+  }, []);
+
+const router = useRouter();  
   return (
-    <div className="relative isolate overflow-hidden bg-white">
-      <svg
-        className="absolute inset-0 -z-10 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
-        aria-hidden="true"
-      >
-        <defs>
-          <pattern
-            id="0787a7c5-978c-4f66-83c7-11c213f99cb7"
-            width={200}
-            height={200}
-            x="50%"
-            y={-1}
-            patternUnits="userSpaceOnUse"
-          >
-            <path d="M.5 200V.5H200" fill="none" />
-          </pattern>
-        </defs>
-        <rect
-          width="100%"
-          height="100%"
-          strokeWidth={0}
-          fill="url(#0787a7c5-978c-4f66-83c7-11c213f99cb7)"
-        />
-      </svg>
-      
-      <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40">
-        <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl lg:flex-shrink-0 lg:pt-8">
-          <div className="mt-24 sm:mt-32 lg:mt-16">
-            <a href="#" className="inline-flex space-x-6">
-              <span className="rounded-full bg-sky-600/10 px-3 py-1 text-sm font-semibold leading-6 text-sky-600 ring-1 ring-inset ring-sky-600/10">
-                New version
-              </span>
-              <span className="inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-600">
-                <span>Just shipped v1.0</span>
-              </span>
-              <Link href="/sign-in">
-                <Button>
-                  Login
-                </Button>
-              </Link>
-              <Link href="/sign-up">
-                <Button>
-                  Register
-                </Button>
-              </Link>
-            </a>
-          </div>
-          <h1 className="mt-10 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-            Get your questions answered with DOCTALK
-          </h1>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
-            Have a better doctors appointment
-          </p>
+    <div>
+      {/* Include the Calendly badge widget */}
+      <link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet" />
 
-          <div className="mt-10 flex items-center gap-x-6">
-            <Link href="/sign-up"
-              className="rounded-md bg-sky-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
-            >
-              Get started
-            </Link>
-            <Link
-              href="/sign-up"
-              className="text-sm font-semibold leading-6 text-gray-900"
-            >
-              Learn more <span aria-hidden="true">→</span>
-            </Link>
-          </div>
-        </div>
-        <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
-          <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
-            <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-              <img
-                src="/LandingPage.png"
-                alt="App screenshot"
-                width={2432}
-                height={1442}
-                className="w-[76rem] rounded-md shadow-2xl ring-1 ring-gray-900/10"
-              />
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Get your questions answered
+        </h2>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          Chat with your personal assistant
+        </p>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool) => (
+          <Card
+            onClick={() => router.push(tool.href)}
+            key={tool.href}
+            className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+          >
+            <div className="flex items-center gap-x-4">
+              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                <tool.icon className={cn("w-8 h-8", tool.color)}/>
+              </div>
             </div>
-          </div>
-        </div>
+            <div className="font-semibold">
+              {tool.label}
+            </div>
+            <ArrowRight className="w-5 h-5" />
+          </Card>
+        ))}
       </div>
     </div>
-  );
+  )
 }
+export default DashboardPage;
